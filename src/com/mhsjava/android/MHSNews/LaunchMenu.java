0df0accee6d1158f293190f0b10e76a7e5438dbf
Ben Shepherd <8bitben@gmail.com>
2012-09-20 20:15:33 -0400

    Initial Upload
    First upload of all basic template files

---
+++ src/com/mhsjava/android/MHSNews/LaunchMenu.java
@@ -0,0 +1,20 @@
+package com.mhsjava.android.MHSNews;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LaunchMenu extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_launch_menu, menu);
+        return true;
+    }
+}
